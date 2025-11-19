28128bab88f484aa954cdb22dc10f8771c36733f
il-Ke <204044211@qq.com>
2025-11-19 23:01:02 +0800

    Refactor guessing game logic with binary search

--- the_first_work.py
+++ the_first_work.py
@@ -1,13 +1,17 @@
 n=int(input())
-a=50
+l=0
+r=100
+mid=-1
 cnt=0
-while a!=n:
-    a=int((a+n)/2)
+while mid!=n:
+    mid=int((l+r)/2)
     cnt+=1
-    if a>n:
-        print(f"{a} 大")
-    elif a<n:
-        print(f"{a} 小")
+    if mid>n:
+        print(f"{mid} 大")
+        r=mid
+    elif mid<n:
+        print(f"{mid} 小")
+        l=mid+1
 print(f"{n} 正确")
 
 print(f"最终猜了{cnt}次")

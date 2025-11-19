d4f0bc7fa512c4acaa42f00030a699cd3a6765ec
il-Ke <204044211@qq.com>
2025-11-19 22:54:14 +0800

    Change guessing method to binary search
    Refactor guessing logic to use binary search approach.

--- the_first_work.py
+++ the_first_work.py
@@ -1,12 +1,13 @@
 n=int(input())
-a=0
+a=50
 cnt=0
 while a!=n:
-    a=int(input())
+    a=int((a+n)/2)
     cnt+=1
     if a>n:
         print(f"{a} 大")
     elif a<n:
         print(f"{a} 小")
 print(f"{n} 正确")
+
 print(f"最终猜了{cnt}次")
